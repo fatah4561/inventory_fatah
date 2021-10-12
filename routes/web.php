d37be474291187c48759927fae311ee96b384f28
inventory_fatah <fatahathariq6@gmail.com>
2021-10-12 14:12:30 +0700

    belajar routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,68 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return "Hello World";
+});
+Route::get('/name/{name}', function ($name) {
+    return "Hello {$name}";
+});
+Route::get('/data/{data?}', function ($data = "Kosong") {
+    return "Isi parameter = {$data}";
+});
+Route::get('/name/{name}', function ($name) {
+    return "Nama = {$name}";
+})->where('name', '[A-Za-z]+');
+// studi kasus
+// Route::get('/{NRP}&{Nama}', function ($NRP, $Nama) {
+//     if(is_numeric($NRP) && ctype_alpha($Nama)){
+//         return "NRP = {$NRP}, Nama = {$Nama}";
+//     }elseif(is_numeric($NRP) && !ctype_alpha($Nama)){
+//         return "NRP = {$NRP} dan Nama bukan alphabet";
+//     }elseif(!is_numeric($NRP) && ctype_alpha($Nama)){
+//         return "NRP bukan angka dan Nama = {$Nama}";
+//     }else{
+//         return "NRP bukan angka dan nama bukan alphabet";
+//     }
+// });
+Route::get('/{NRP}&{Nama}', function ($NRP, $Nama) {
+    return "NRP = {$NRP}, Nama = {$Nama}";
+
+})-> where(['name' => '[A-Za-z]+', 'NRP' => '[0-9]+'
+]);;
+
+Route::get('/cekbilangan/{bilangan}', function ($bilangan) {
+    if($bilangan %2 == 0){
+        return "{$bilangan} merupakan genap";
+    }else{
+        return "{$bilangan} merupakan ganjil";
+    }
+});
+Route::get('/deretbilangan/{bilangan}', function ($bilangan) {
+    for ($i = 0; $i <= $bilangan; $i++){
+        if($i % 2 == 0){
+            echo "{$i} genap";
+            echo "</br>";
+        }else{
+            echo "{$i} ganjil";
+            echo "</br>";
+        }
+    }
+});
+Route::get('/logika/{awal}&{akhir}', function ($awal, $akhir) {
+    if($awal > $akhir){
+        return "Data Awal Tidak Boleh Lebih Besar";
+    }else{
+        $angka_awal = 0;
+        echo "{$angka_awal}";
+        echo "<br/>";
+        for ($i = $awal; $i <= $akhir; $i+$angka_awal){
+            $angka_sebelumnya = $i;
+            $angka_sekarang = $angka_awal + $angka_sebelumnya;
+            echo "{$angka_sekarang}";
+            echo "<br/>";
+
+            $angka_awal = $angka_sebelumnya;
+        }
+    }
+});
